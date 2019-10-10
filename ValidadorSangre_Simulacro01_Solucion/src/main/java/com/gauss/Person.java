f4043d2e2587245e6534458272dc5507f60d5e55
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2019-10-10 11:08:00 +0200

    Arregla fallos simulacro 1

---
+++ ValidadorSangre_Simulacro01_Solucion/src/main/java/com/gauss/Person.java
@@ -0,0 +1,54 @@
+package com.gauss;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
+
+public class Person {
+	private LocalDate birthDate;
+	private List<BloodAnalysis> analysisList;
+
+	public Person() {
+		this.analysisList = new ArrayList<BloodAnalysis>();
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public void addBirthDate(int day, int month, int year) {
+		this.birthDate = new LocalDate(year, month, day);
+	}
+
+	public int getAge() {
+		return Years.yearsBetween(birthDate, new LocalDate()).getYears();
+	}
+
+	public List<BloodAnalysis> getAnalysis() {
+		return analysisList;
+	}
+
+	public void addBloodAnalysis(BloodAnalysis analysis) {
+		this.analysisList.add(analysis);
+		analysis.setPerson(this);
+	}
+
+	public void show() {
+		String message = "Analisis realizados:\n";
+		for (BloodAnalysis analysis : analysisList) {
+			message += analysis.toString();
+		}
+		System.out.println(message);
+	}
+
+	@Override
+	public String toString() {
+		return "Person [age=" + getAge() + "]";
+	}
+
+}
